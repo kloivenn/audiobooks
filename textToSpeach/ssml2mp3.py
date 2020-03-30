b555245244874f94dcd8e00b994741d29db64b30
Svetlana <s.ovchinnikova@zmbh.uni-heidelberg.de>
2020-03-30 13:51:37 +0200

    New text

--- textToSpeach/ssml2mp3.py
+++ textToSpeach/ssml2mp3.py
@@ -32,7 +32,7 @@ voice = args.voice or "Salli"
 #voice = "Joanna"
 
 infile = args.infile
-i = 459
+i = 288
 n_pieces = 9
 
 pieces = []
@@ -40,7 +40,7 @@ with open(infile, "rb") as f:
     pieces = [l for l in (line.strip() for line in f) if l]
 
 count = 0
-file_index = 52
+file_index = 33
 
 # Do for all pieces
 while i < len(pieces):
